8665a1c2987fac47f75a805d44668f9c57cd2955
erieger11 <erieger530@gmail.com>
2024-05-29 00:32:49 -0400

    fixed the batter getting contact methods

--- src/main/java/com/gamblinggenie/PassionProjectGamblingGenie/Service/SimulationService.java
+++ src/main/java/com/gamblinggenie/PassionProjectGamblingGenie/Service/SimulationService.java
@@ -7,23 +7,34 @@ import java.util.Random;
 @Service
 public class SimulationService {
 
-    static int outs = 0;
-    static int strikes = 0;
-    static int balls = 0;
-    Boolean pitchContact = false;
-
+    private int homeTeamScore = 0;
+    private int awayTeamScore = 0;
+    private double inning = 1;
+    private int outs = 0;
+    private int strikes = 0;
+    private int balls = 0;
+    private boolean pitchContact = false;
 
     public String baseballGameInning() {
-
+        pitchContact = false;
         String pitchOutcome = pitchResult();
         String atBatOutcome = atBatResult();
-        String hitOutcome = hitOutcome();
-        if(pitchContact){
-            return pitchOutcome + " " + hitOutcome;
+        String hitOutcome = "";
+        if (pitchContact) {
+            hitOutcome = hitOutcome();
         }
-        else{
-            return pitchOutcome + " " + atBatOutcome;
+        if (outs == 3) {
+            outs = 0;
+            inning += 0.5;
         }
+        if (inning >= 9) {
+            return "GAME OVER\nHome : " + homeTeamScore + " - " + "Away : " + awayTeamScore;
+        }
+        return "Home : " + homeTeamScore + " - " + "Away : " + awayTeamScore + "\n" +
+                "Inning : " + inning + "\n" +
+                pitchOutcome + "\n" +
+                atBatOutcome + "\n" +
+                hitOutcome;
     }
 
     public String atBatResult() {
@@ -45,20 +56,23 @@ public class SimulationService {
         Random rand = new Random();
         int number = rand.nextInt(4);
 
-        if (number == 0) {
-            balls++;
-            return "Pitch was a ball. Pitch count is " + balls + "-" + strikes + " and " + outs + " outs.";
-        } else if (number == 1) {
-            strikes++;
-            return "Pitch was a strike.  Pitch count is " + balls + "-" + strikes + " and " + outs + " outs.";
-        } else if (number == 2){
-            pitchContact = true;
-            return "Pitch contact was made.  Pitch count is " + balls + "-" + strikes + " and " + outs + " outs.";
-        } else {
-            if (strikes < 2) {
+        switch (number) {
+            case 0:
+                balls++;
+                return String.format("Pitch was a ball. Pitch count is %d-%d and %d outs.", balls, strikes, outs);
+            case 1:
                 strikes++;
-            }
-            return "Pitch was a foul ball. Pitch count is " + balls + "-" + strikes + " and " + outs + " outs.";
+                return String.format("Pitch was a strike. Pitch count is %d-%d and %d outs.", balls, strikes, outs);
+            case 2:
+                if (strikes < 2) {
+                    strikes++;
+                }
+                return String.format("Pitch was a foul ball. Pitch count is %d-%d and %d outs.", balls, strikes, outs);
+            case 3:
+                pitchContact = true;
+                return String.format("Pitch contact was made. Pitch count is %d-%d and %d outs.", balls, strikes, outs);
+            default:
+                return "Unexpected pitch outcome.";
         }
     }
 
@@ -67,77 +81,40 @@ public class SimulationService {
         int num = ballPlacement.nextInt(100);
         strikes = 0;
         balls = 0;
-        if (num <= 4) {
-            outs++;
-            return "Fly out to deep left";
-        } else if (num >= 5 && num <= 8) {
-            outs++;
-            return "Fly out to deep center";
-        } else if (num >= 9 && num <= 12) {
-            outs++;
-            return "Fly out to deep right";
-        } else if (num >= 13 && num <= 16) {
-            outs++;
-            return "Fly out to shallow left";
-        } else if (num >= 17 && num <= 20) {
-            outs++;
-            return "Fly out to shallow center";
-        } else if (num >= 21 && num <= 24) {
-            outs++;
-            return "Fly out to shallow right";
-        } else if (num >= 25 && num <= 27) {
-            outs++;
-            return "Fly out behind catcher";
-        } else if (num >= 28 && num <= 31) {
-            outs++;
-            return "Foul out right";
-        } else if (num >= 32 && num <= 35) {
-            outs++;
-            return "Foul out left";
-        } else if (num >= 36 && num <= 39) {
-            outs++;
-            return "Ground out to 1B";
-        } else if (num >= 40 && num <= 43) {
-            outs++;
-            return "Ground out to 2B";
-        } else if (num >= 44 && num <= 47) {
-            outs++;
-            return "Ground out to SS";
-        } else if (num >= 48 && num <= 51) {
-            outs++;
-            return "Ground out to 3B";
-        } else if (num >= 52 && num <= 55) {
-            outs++;
-            return "Ground out to P";
-        } else if (num >= 56 && num <= 60) {
-            return "Single to shallow right";
-        } else if (num >= 61 && num <= 65) {
-            return "Single to shallow center";
-        } else if (num >= 66 && num <= 70) {
-            return "Single to shallow left";
-        } else if (num >= 71 && num <= 75) {
-            return "Single to deep right";
-        } else if (num >= 76 && num <= 80) {
-            return "Single to deep center";
-        } else if (num >= 81 && num <= 85) {
-            return "Single to deep left";
-        } else if (num >= 86 && num <= 88) {
-            return "Double to deep right";
-        } else if (num >= 89 && num <= 91) {
-            return "Double to deep center";
-        } else if (num >= 92 && num <= 94) {
-            return "Double to deep left";
-        } else if (num >= 95 && num <= 96) {
-            return "Triple to deep right";
-        } else if (num >= 97 && num <= 98) {
-            return "Triple to deep center";
-        } else if (num == 99) {
-            return "Triple to deep left";
-        } else {
-            return "HOMERUN";
-        }
+
+        if (num <= 4) return recordOut("Fly out to deep left");
+        if (num <= 8) return recordOut("Fly out to deep center");
+        if (num <= 12) return recordOut("Fly out to deep right");
+        if (num <= 16) return recordOut("Fly out to shallow left");
+        if (num <= 20) return recordOut("Fly out to shallow center");
+        if (num <= 24) return recordOut("Fly out to shallow right");
+        if (num <= 27) return recordOut("Fly out behind catcher");
+        if (num <= 31) return recordOut("Foul out right");
+        if (num <= 35) return recordOut("Foul out left");
+        if (num <= 39) return recordOut("Ground out to 1B");
+        if (num <= 43) return recordOut("Ground out to 2B");
+        if (num <= 47) return recordOut("Ground out to SS");
+        if (num <= 51) return recordOut("Ground out to 3B");
+        if (num <= 55) return recordOut("Ground out to P");
+
+        if (num <= 60) return "Single to shallow right";
+        if (num <= 65) return "Single to shallow center";
+        if (num <= 70) return "Single to shallow left";
+        if (num <= 75) return "Single to deep right";
+        if (num <= 80) return "Single to deep center";
+        if (num <= 85) return "Single to deep left";
+        if (num <= 88) return "Double to deep right";
+        if (num <= 91) return "Double to deep center";
+        if (num <= 94) return "Double to deep left";
+        if (num <= 96) return "Triple to deep right";
+        if (num <= 98) return "Triple to deep center";
+        if (num == 99) return "Triple to deep left";
+
+        return "HOMERUN";
     }
 
-
+    private String recordOut(String outcome) {
+        outs++;
+        return outcome;
+    }
 }
-
